cd6292be4f91948742de8f7ec7e5214b4b99638b
Royden Rego <roydenrego@softrixz.com>
2020-02-23 00:06:31 +0530

    Integrated portfolio and contact with Lambda Functions

--- src/components/landing-page/ContactSection/index.jsx
+++ src/components/landing-page/ContactSection/index.jsx
@@ -1,7 +1,101 @@
 import React, { Component } from 'react';
 import './style.css';
 
+import axios from 'axios';
+import ReCAPTCHA from "react-google-recaptcha";
+import Swal from 'sweetalert2';
+
 export default class DownloadSection extends Component {
+
+    state = {
+        buttonEnabled: false
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.recaptchaRef = React.createRef();
+    }
+
+    async contact(fullname, email, message, captcha) {
+        this.setState({
+            buttonEnabled: false
+        });
+        try {
+            const response = await axios({
+                url: 'http://localhost:8010/proxy/v1/contact',
+                method: 'POST',
+                auth: {
+                    username: 'ltmFPInUvF',
+                    password: '9PYpR3jw0JlZOQ5JEpBE'
+                },
+                data: {
+                    fullname,
+                    email,
+                    message,
+                    'g-recaptcha-response': captcha
+                }
+            });
+            console.log(response);
+            if (response.data.status === 'ok') {
+                this.setState({
+                    buttonEnabled: true,
+                    name: '',
+                    email: '',
+                    message: ''
+                });
+                Swal.fire({
+                    title: 'Sent',
+                    text: 'Your message has been sent successfully. I will reach out to you very soon.',
+                    icon: 'success'
+                });
+            }
+            else {
+                Swal.fire({
+                    title: 'Error',
+                    message: 'Something went wrong. Please try again after sometime',
+                    icon: 'error'
+                });
+            }
+        }
+        catch (e) {
+            console.log(e);
+        }
+    };
+
+    onChange = () => {
+        this.setState({
+            buttonEnabled: true
+        });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.contact(this.state.name,
+            this.state.email,
+            this.state.message,
+            this.recaptchaRef.current.getValue());
+    };
+
+    nameChange = (e) => {
+        this.setState({
+            name: e.target.value
+        })
+    }
+
+    emailChange = (e) => {
+        this.setState({
+            email: e.target.value
+        })
+    }
+
+    messageChange = (e) => {
+        this.setState({
+            message: e.target.value
+        })
+    }
+
+
     render() {
         return (
             <section id="contact" className="parallax-section">
@@ -16,23 +110,42 @@ export default class DownloadSection extends Component {
                                 </div>
 
                                 <div id="contact-form">
-                                    <form action="/submit" method="post" id="contactForm">
+                                    <form action="#" method="post" id="contactForm" onSubmit={this.handleSubmit}>
                                         <div className="wow fadeInUp" data-wow-delay="1s">
-                                            <input name="fullname" type="text" className="form-control" id="fullname" placeholder="Your Name" required />
+                                            <input name="fullname" type="text" className="form-control"
+                                                id="fullname" placeholder="Your Name"
+                                                value={this.state.name}
+                                                onChange={this.nameChange}
+                                                required />
                                         </div>
 
                                         <div className="wow fadeInUp" data-wow-delay="1.2s">
-                                            <input name="email" type="email" className="form-control" id="email" placeholder="Your Email" required />
+                                            <input name="email" type="email" className="form-control"
+                                                id="email" placeholder="Your Email"
+                                                value={this.state.email}
+                                                onChange={this.emailChange} required />
                                         </div>
                                         <div className="wow fadeInUp" data-wow-delay="1.4s">
-                                            <textarea name="message" rows="5" className="form-control" id="message" placeholder="Write your message..." required></textarea>
+                                            <textarea name="message" rows="5" className="form-control"
+                                                id="message" placeholder="Write your message..."
+                                                onChange={this.messageChange}
+                                                value={this.state.message}
+                                                required/>
                                         </div>
 
-                                        <div className="g-recaptcha wow fadeInUp" data-sitekey="6LdVVYQUAAAAAKyM6ST5JMi4lwNXM13B-11U9Ry8" data-callback="recaptchaCallback"
-                                            data-wow-delay="1.6s"></div>
+                                        <ReCAPTCHA
+                                            ref={this.recaptchaRef}
+                                            sitekey="6LdVVYQUAAAAAKyM6ST5JMi4lwNXM13B-11U9Ry8"
+                                            onChange={this.onChange}
+                                            data-wow-delay="1.6s" />
 
                                         <div className="wow fadeInUp col-md-6 col-sm-8" data-wow-delay="1.8s">
-                                            <input name="submit" type="submit" className="form-control grc-disabled" id="submit" value="Send" disabled />
+                                            {this.state.buttonEnabled ?
+                                                <input name="submit" type="submit" className="form-control" id="submit" value="Send" />
+                                                :
+                                                <input name="submit" type="submit" className="form-control grc-disabled" id="submit" value="Send" disabled />
+                                            }
+
                                         </div>
                                     </form>
                                 </div>
