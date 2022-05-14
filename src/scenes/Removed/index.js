ce7875fa57ec4f989e21ddb300361c73b16438fe
Royden Rego <roydenrego@softrixz.com>
2022-05-14 16:27:00 +0530

    Update index.js

--- src/scenes/Removed/index.js
+++ src/scenes/Removed/index.js
@@ -5,7 +5,7 @@ export default function Removed() {
         <div style={{ textAlign: "center" }}>
             <h1>Notice</h1>
             <p>Hello there, this is Royden. It is with great regret I inform you that I have taken down my site and it will no longer be available. /s</p>
-            <p>If needed, you can reach out to me at <span>royden</span>rego4@<span>gmail.com</span>.</p>
+            <p>If needed, you can reach out to me at <span>me</span>@<span>roydenrego.com</span>.</p>
         </div>
     );
 }
